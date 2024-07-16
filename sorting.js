1de8af7038051f591ead61ff40be1562dda42ddd
ramakrishnarao679 <ramakrishnarao679@gmail.com>
2024-07-16 14:00:23 +0530

    add sorting

---
+++ sorting.js
@@ -0,0 +1,13 @@
+function customsort(arr){
+    const len =arr.length;
+    for(let i=0; i<len-1; i++){
+        for(let j=0; j<len-1; j++){
+            if(arr[j]>arr[j+1]){
+                const temp = arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+    return arr;
+};
